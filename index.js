70f3555fda1e5860b00b45cdf19473364a38dc13
ElodieDenis <denis.el@laposte.net>
2025-04-09 19:40:14 +0200

    création regex password

--- index.js
+++ index.js
@@ -23,21 +23,6 @@ closeBtn.addEventListener("click", () => {
 });
 
 // --------------------------------------------------------------
-// Faire apparaît le mot de passe ainsi que changer l'image de l'oeil dans la section de connection
-const toggleBtn = document.querySelector(".toggle_password");
-const passwordInput = document.querySelector(".div_password input");
-const eyeIcon = toggleBtn.querySelector(".img_eyes_connection");
-
-toggleBtn.addEventListener("click", () => {
-  const isPassword = passwordInput.type === "password";
-  passwordInput.type = isPassword ? "text" : "password";
-
-  eyeIcon.src = isPassword
-    ? "/assets/eye_open_password.png"
-    : "/assets/eye_close_password.png";
-});
-
-// --------------------------------------------------------------
 //  Apparition de la compte pour créer un nouveau compte
 const aCreateAccount = document.querySelector(".a_create_account");
 const overlayCreateAccount = document.querySelector(".overlay_create_account");
@@ -55,45 +40,63 @@ closeBtnCreateAccount.addEventListener("click", () => {
 });
 
 // --------------------------------------------------------------
-// Faire apparaît le mot de passe ainsi que changer l'image de l'oeil dans la section de création d'un nouveau compte
-const divPasswordInput = document.querySelector(
-  ".div_password_create_account input"
-);
-console.log(divPasswordInput);
-const togglePassword = document.querySelector(
-  ".toggle_password_create_account"
-);
-const imgEyesCreateAccount = document.querySelector(".img_eyes_create_account");
+// Changer l'image de l'oeil au clique sur ce dernier en fonction du type de l'input
+const passwords = document.querySelectorAll(".password");
+const imgEyes = document.querySelectorAll(".img_eyes");
 
-togglePassword.addEventListener("click", () => {
-  const isPasswordCreateAccount = divPasswordInput.type === "password";
-  divPasswordInput.type = isPasswordCreateAccount ? "text" : "password";
+imgEyes.forEach((imgEye) => {
+  imgEye.addEventListener("click", () => {
+    const eyes = imgEye.querySelector("img");
+    const container = imgEye.closest(".champ_password");
 
-  imgEyesCreateAccount.src = divPasswordInput
-    ? "/assets/eye_open_password.png"
-    : "/assets/eye_close_password.png";
+    const passwordNew = container.querySelector(".password");
+
+    if (passwordNew.type === "password") {
+      passwordNew.type = "text";
+      eyes.src = "/assets/eye_open_password.png";
+      eyes.alt = "Masquer le mot de passe";
+    } else {
+      passwordNew.type = "password";
+      eyes.src = "/assets/eye_close_password.png";
+      eyes.alt = "Afficher le mot de passe";
+    }
+  });
 });
 
 // --------------------------------------------------------------
-// // Faire apparaît le mot de passe ainsi que changer l'image de l'oeil dans la section de création d'un nouveau compte confirmation du mot de passe
-const divPasswordConfirmation = document.querySelector(
-  ".div_password_confirmation input"
-);
-const togglePasswordConfirmation = document.querySelector(
-  ".toggle_password_confirmation"
-);
-const imgEyesCreateAccountConfirmation = document.querySelector(
-  ".img_eyes_create_account_confirmation"
-);
+// Au clic sur les boutons fermés toutes les valeurs des inputs sont supprimés et les oeil des mot de passe changés ainsi que le type de input qui repassent en password
+const btnsClose = document.querySelectorAll(".btn_close");
+const inputs = document.querySelectorAll(".input");
+const eyesNew = document.querySelectorAll(".eyes");
 
-togglePasswordConfirmation.addEventListener("click", () => {
-  const isPasswordCreateAccountConfirmation =
-    divPasswordConfirmation.type === "password";
-  divPasswordConfirmation.type = isPasswordCreateAccountConfirmation
-    ? "text"
-    : "password";
+btnsClose.forEach((btnClose) => {
+  btnClose.addEventListener("click", () => {
+    eyesNew.forEach((eyeNew) => {
+      eyeNew.attributes.src.nodeValue = "/assets/eye_close_password.png";
+      eyeNew.attributes.alt.nodeValue = "Afficher le mot de passe";
+    });
+    inputs.forEach((input) => {
+      input.value = "";
+      input.attributes.type.nodeValue = "password";
+    });
+  });
+});
 
-  imgEyesCreateAccountConfirmation.src = divPasswordConfirmation
-    ? "/assets/eye_open_password.png"
-    : "/assets/eye_close_password.png";
+// Ajouter les conditions obligatoires pour les mot de passe (majsucule, minuscule, chiffre, caractères spéciaux)
+const passwordOverlay = document.querySelector(".password_input_overlay");
+console.log(passwordOverlay);
+
+const btnConnection = document.querySelector(".btn_connection");
+console.log(btnConnection);
+
+btnConnection.addEventListener("click", () => {
+  const passwordValid = passwordOverlay.value;
+
+  const regexPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).+$/.test(passwordValid);
+  if (regexPasswordValid) {
+    console.log("Mot de passe valide");
+  } else {
+    console.log("Mot de passe invalide");
+  }
 });
